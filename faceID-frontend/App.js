a88cbc2e417adf71e3e3dceb5eafff0c34299b8b
Huyen Pham <thanhhuyen2995@gmail.com>
2020-05-17 16:35:53 -0700

    video retrieved, in progress of sending to server

--- faceID-frontend/App.js
+++ faceID-frontend/App.js
@@ -5,8 +5,13 @@ import { render } from 'react-dom';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
-
+  const [recording, setRecording] = useState(false);
+  const [cameraRef, setCameraRef] = useState(null);
+  const options = {
+    mute: true,
+    maxDuration: 3,
+  }
+  
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
@@ -26,8 +31,34 @@ export default function App() {
       <Text style={styles.title}> Face ID </Text>
       
       <View style={styles.cameraContainer}>
-        <Camera style={styles.camera} type={Camera.Constants.Type.front}></Camera>
-        <TouchableOpacity style={styles.recorderButton}></TouchableOpacity>
+        <Camera style={styles.camera} type={Camera.Constants.Type.front} ref={ref => {
+          setCameraRef(ref);
+        }}></Camera>
+        <TouchableOpacity style={recording ? styles.recorderButtonStarted : styles.recorderButton}
+        onPress={async() => {
+            setRecording(true);
+            const video = await cameraRef.recordAsync(options);
+            //console.log('video', video);
+            setRecording(false);
+
+            //uploading vid to server
+            const data = new FormData();
+            //console.log(video.uri);
+            data.append('uri', video.uri);
+            data.append('vid-name', Date.now().toString());
+
+            fetch('https://c2344539.ngrok.io/', {
+              method: 'POST',
+              body: data,
+            }).then((response) => {
+              response.json().then((body) => {
+                  console.log(body.uri);
+              });
+            });
+
+            setRecording(false);
+        }}>
+        </TouchableOpacity>
       </View>
 
       <Text style={styles.instructionText}> Please face the camera and press on recording button </Text>
@@ -36,6 +67,7 @@ export default function App() {
   );
   
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -64,6 +96,16 @@ const styles = StyleSheet.create({
     height: 60,
     borderRadius: 50,
     backgroundColor: 'red',
+    alignItems: 'center',
+  },
+  recorderButtonStarted: {
+    marginTop: 15,
+    borderWidth: 5,
+    borderColor: 'white',
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    backgroundColor: 'black',
     alignItems: 'center',
   },
   camera: {
