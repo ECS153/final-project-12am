c13b644b9a3da91511bf4b463c196f59d977de07
linda413 <caiqinli99@yahoo.com.tw>
2020-05-18 13:15:49 -0700

    .

--- backend/server/app.py
+++ backend/server/app.py
@@ -1,12 +1,11 @@
 from flask import Flask
-
+from utils import *
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+
     return "hello world!"
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ backend/server/utils.py
@@ -0,0 +1,40 @@
+import cv2 
+import os 
+# Importing all necessary libraries 
+def extract_image():
+	# Read the video from specified path 
+	cam = cv2.VideoCapture("C:\\Users\\caiqi\\Downloads\\VID_20200517_195111.mp4") 
+	try: 		
+		# creating a folder named data 
+		if not os.path.exists('data'): 
+			os.makedirs('data') 
+
+	# if not created then raise error 
+	except OSError: 
+		print ('Error: Creating directory of data') 
+
+	# frame 
+	currentframe = 0
+
+	while(True): 
+		
+		# reading from frame 
+		ret,frame = cam.read() 
+
+		if ret: 
+			# if video is still left continue creating images 
+			name = './data/frame' + str(currentframe) + '.jpg'
+			print ('Creating...' + name) 
+
+			# writing the extracted images 
+			cv2.imwrite(name, frame) 
+
+			# increasing counter so that it will 
+			# show how many frames are created 
+			currentframe += 1
+		else: 
+			break
+
+	# Release all space and windows once done 
+	cam.release() 
+	cv2.destroyAllWindows() 
