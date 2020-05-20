1e6fefffc4c0d4e7963f8ede0a9410e0dfa46181
Huyen Pham <thanhhuyen2995@gmail.com>
2020-05-19 22:04:03 -0700

    added server

--- backend/server/app.py
+++ backend/server/app.py
@@ -1,18 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 CORS(app)
+
+ALLOWED_EXTENSIONS = set(['mov','mp4'])
 
 @app.route('/')
 def index():
     return "hello world!"
 
 @app.route('/upload', methods=['GET','POST'])
-@cross_origin(origin='*',headers=['Content- Type'])
-def testFunc():
-    print("bla")
-    return "hahahahaa"
+def fileUpload():
+    print(request.files['file'])
+    return "Done"
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
