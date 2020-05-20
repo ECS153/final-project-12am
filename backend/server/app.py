9045295ba99287361d6fa47498474f27d3897478
Kenny Li <kennyli@KennydeMacBook-Pro.local>
2020-05-19 20:44:46 -0700

    refactor with Linda

--- backend/server/app.py
+++ backend/server/app.py
@@ -1,75 +1,62 @@
-
-from utils import get_frames
-
+from utils import (
+    get_frames,
+    delete_frames,
+)
 from flask import (
     Flask,
     render_template,
 )
-import os
-import config
 from analyzer import Analyzer
+import os
 
 app = Flask(__name__)
 
+
 # Set environment variables
-# Flask App
-# os.environ['FLASK_APP'] = 'app.py'
-# os.environ['FLASK_ENV'] = 'development'
+os.environ['FLASK_APP'] = 'app.py'
+os.environ['FLASK_ENV'] = 'development'
 
-# # MS Face Recognition
-# os.environ['FACE_SUBSCRIPTION_KEY'] = '50aaf75a5e464e1abb264a7aec7414c2'
-# os.environ['FACE_ENDPOINT'] = 'https://mycsresourceface.cognitiveservices.azure.com/'
 
-# FLASK_APP = config('FLASK_APP')
-# FLASK_ENV = config('FLASK_ENV')
+# TODO: Configure for training/testing
+user_name = "Linda"
+analyzer = Analyzer(user_name)
+THRESHOLD = 0.50
 
 
 @app.route('/')
 def index():
     print("start")
-    # Trained, Identify ONLY
-    # Person we are identifying (for testing purposes for now
-    # since multiple people working on this)
-    name = "Linda"
-    analyzer = Analyzer(name)
-    # get_frames(name)
-    # Identify Only
-    analyzer.identify()
-    # analyzer.delete()
-    # analyzer.verify()
-    return "Good morning world!"
+    get_frames(user_name)
+    confidence = analyzer.identify()
+    analyzer.detect_liveness()
+    delete_frames()
+    if analyzer.detect_liveness() and confidence > THRESHOLD:
+        return 'Detect result: True'
+    return 'Detect result: False'
 
 
 @app.route('/create')
 def create():
-    analyzer = Analyzer("Linda", 0)
     analyzer.create()
-    return "Created Person Group"
+    return "Created Person Group."
 
 
 @app.route('/train')
 def train():
-    name = "Linda"
-    analyzer = Analyzer(name, 0)
     # Train with the videos upload
-    # get_frames()
+    get_frames(name)
     # Detect faces from the frames and add to Person Group
     analyzer.get_train_data()
     # Use the frames in the person group to train
-    analyzer.train_data()
-    # analyzer.identify()
-    # analyzer.verify()
-    return "Good morning world!"
+    analyzer.train()
+    return "Trained Person Group."
+
 
 @app.route('/delete')
 def delete():
-    name = "Linda"
-    analyzer = Analyzer(name, 0)
     analyzer.delete()
-    return "Good morning world!"
+    return "Deleted Person Group."
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
