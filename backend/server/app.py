7e605e35366ac14e411e0d3d9c5c9bf9f7963231
Kenny Li <kennyli@KennydeMacBook-Pro.local>
2020-05-25 16:04:49 -0700

    finish upload

--- backend/server/app.py
+++ backend/server/app.py
@@ -24,56 +24,35 @@ os.environ['FLASK_ENV'] = 'development'
 
 app.config["UPLOAD_FOLDER"] = './media/test'
 ALLOWED_EXTENSIONS = {'mov', 'mp4'}
-
-# TODO: Configure for training/testing
-user_name = "Linda"
-analyzer = Analyzer(user_name)
 THRESHOLD = 0.50
-
-
-def detect():
-    print("start")
-    get_frames(user_name)
-    analyzer.create()
-    analyzer.train()
-    confidence = analyzer.identify()
-    delete_frames()
-    analyzer.delete()
-    if confidence > THRESHOLD:
-        return 'Detect result: True'
-    # if analyzer.detect_liveness() and confidence > THRESHOLD:
-    #     return 'Detect result: True'
-    return 'Detect result: False'
 
 
 @app.route('/')
 def index():
-    #detect()
     return 'Welcome to Lively'
 
 
-@app.route('/create')
-def create():
-    analyzer.create()
-    return "Created Person Group."
+# @app.route('/create')
+# def create():
+#     analyzer.create()
+#     return "Created Person Group."
+#
+#
+# @app.route('/train')
+# def train():
+#     # Train with the videos upload
+#     get_frames(user_name)
+#     # Detect faces from the frames and add to Person Group
+#     analyzer.get_train_data()
+#     # Use the frames in the person group to train
+#     analyzer.train()
+#     return "Trained Person Group."
 
 
-@app.route('/train')
-def train():
-    # Train with the videos upload
-    get_frames(user_name)
-    # Detect faces from the frames and add to Person Group
-    analyzer.get_train_data()
-    # Use the frames in the person group to train
-    analyzer.train()
-    return "Trained Person Group."
-
-
-@app.route('/delete')
-def delete():
-    analyzer.delete()
-    return "Deleted Person Group."
-
+# @app.route('/delete')
+# def delete():
+#     analyzer.delete()
+#     return "Deleted Person Group."
 
 
 def allowed_file(filename):
@@ -99,13 +78,23 @@ def file_upload():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             print('DEBUG: file saved!',filename)
             path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            analyzer.detect_liveness(path)
-            # detect()
-            # return redirect(request.url)
-            '''
-            Classification and return the result here
-            '''
-            result = {'result': 'True'}
+
+            # Configure for training/testing
+            # person_name = request.files['name']
+            person_name = 'Linda'
+            analyzer = Analyzer(person_name)
+            get_frames(person_name)
+            analyzer.create()
+            analyzer.train()
+            confidence = analyzer.identify()
+            is_lively = analyzer.detect_liveness(path)
+
+            result = {}
+            if confidence > THRESHOLD and is_lively:
+                result['result'] = 'True'
+            else:
+                result['result'] = 'False'
+            analyzer.delete()
             return jsonify(result)
 
     return render_template('upload.html')
